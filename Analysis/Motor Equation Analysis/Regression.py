672734d28397116d6706171b0748588d2b54e19a
Shivam Malviya <46934893+shivammalviya712@users.noreply.github.com>
2020-03-15 22:11:04 +0530

    Motor Equation Analysis Update

---
+++ Analysis/Motor Equation Analysis/Regression.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as seabornInstance
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+df = pd.read_excel('motorDataAnalysis.xlsx')
+print (df.shape)
+
+
